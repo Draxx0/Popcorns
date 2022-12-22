ebffb6c9763021252203e47e739ff17ce20081e0
Draxx0 <william.fort.lmgl@gmail.com>
2022-12-22 22:30:13 +0100

    add icons / footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import "./Footer.css";
+
 const Footer = () => {
- return ( 
-   <>
-     
-  </>
+  return (
+    <footer>
+      <div className="footer-wrapper">
+        <span>Popcorn</span>
+
+        <ul>
+          <li>
+            <a href="https://github.com/Draxx0">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
-}
- 
+};
+
 export default Footer;
